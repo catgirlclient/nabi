b4eb55a84c27df0d60f1b230b8692b9726f6a302
shuuy <hyivines@proton.me>
2024-08-01 13:00:25 -0700

    fix

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -2,8 +2,6 @@ import {createRoot} from "react-dom/client";
 import React, {StrictMode} from "react";
 import {Index} from "./pages";
 
-import "virutal:uno.css";
-
 const container = document.getElementById("root");
 const root = createRoot(container!);
 
